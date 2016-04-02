20741e2136075e0763f15328be08ef20d1e2a566
Takahiro Izuchi <izuchi@impath.jp>
2016-04-02 18:10:33 +0900

    change view source button design

--- styleguide/functions.php
+++ styleguide/functions.php
@@ -109,7 +109,6 @@ echo '<li role="presentation" class="divider"></li><li role="presentation" class
         echo '<h2 class="sg-h2"><a id="sg-'.$filename.'" class="sg-anchor">'.$title.'</a></h2>';
         //echo '<div class="col-md-10 col-md-offset-1">';
         echo '<div class="row"><div class="col-md-8">';
-        echo '<h3 class="sg-h3">Example</h3>';
         include('markup/'.$type.'/'.$file);
         echo '</div>';
          if (file_exists($documentation)) {
@@ -119,7 +118,7 @@ echo '<li role="presentation" class="divider"></li><li role="presentation" class
           echo '</div></div></div>';
         }
         echo '</div><!--/.sg-display-->';
-        echo '<div class="sg-markup-controls"><button class="btn btn-primary sg-btn sg-btn--source" href="#">View Source</button> <a class="sg-btn--top" href="#top">Back to Top</a> </div>';
+        echo '<div class="sg-markup-controls"><button class="btn sg-btn sg-btn--source" href="#">View Source</button> <a class="sg-btn--top" href="#top">Back to Top</a> </div>';
         echo '<div class="sg-source sg-animated">';
         echo '<button class="btn btn-default sg-btn sg-btn--select" href="#" data-clipboard-target="#code'.$code_cnt.'">Copy Source</button>';
         echo '<pre class="prettyprint linenums" id="code'.$code_cnt.'"><code>';
