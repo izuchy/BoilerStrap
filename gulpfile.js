ba32585aea3647e11bc8dd976343d053a7879909
Takahiro Izuchi <izuchi@impath.jp>
2016-04-02 17:40:16 +0900

    copy bootstrap js

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('reload', function(){
     browserSync.reload();
 });
 
-gulp.task("default",['sass','connect-sync'], function() {
+gulp.task("default",['init','sass','connect-sync'], function() {
     gulp.watch("sass/**/*.scss",["sass"]);
     gulp.watch("styleguide/**/*.php",["reload"]);
 });
@@ -31,3 +31,8 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('css/'))
         .pipe(gulp.dest('styleguide/css/'));
 });
+
+gulp.task('init', function() {
+    gulp.src('bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js')
+        .pipe(gulp.dest('styleguide/js'));
+} );
