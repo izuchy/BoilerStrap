24045cf04a7507dec69c30896975d345b592f1c9
井土 貴寛 <izuchi@impath.jp>
2016-04-04 12:56:43 +0900

    add customize-strap.js

--- styleguide/index.php
+++ styleguide/index.php
@@ -59,6 +59,7 @@
 </div>
 <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+<script src="js/customize-strap.js"></script>
 <script src="js/sg-plugins.js"></script>
 <script src="js/sg.js"></script>
 </body>
