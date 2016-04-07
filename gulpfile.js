0b92c028957bece292ecdb0b47da4815ecc2e0b1
Takahiro Izuchi <izuchi@impath.jp>
2016-04-07 14:25:20 +0900

    generate html from php source code for github pages.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect-php');
 var browserSync = require('browser-sync');
+var php2html = require("gulp-php2html");
 
 gulp.task('connect-sync', function() {
     connect.server({
@@ -19,8 +20,9 @@ gulp.task('reload', function(){
 });
 
 gulp.task("default",['init','sass','connect-sync'], function() {
-    gulp.watch("sass/**/*.scss",["sass"]);
-    gulp.watch("styleguide/**/*.php",["reload"]);
+    gulp.watch("sass/**/*.scss",["sass","cp_assets","php2html"]);
+    gulp.watch("js/**/*.js",["cp_assets","php2html"]);
+    gulp.watch("styleguide/**/*.php",["reload","cp_assets","php2html"]);
 });
 
 gulp.task('sass', function () {
@@ -28,8 +30,20 @@ gulp.task('sass', function () {
         .pipe(sass({
             style : 'expanded'
         }))
-        .pipe(gulp.dest('css/'))
-        .pipe(gulp.dest('styleguide/css/'));
+        .pipe(gulp.dest('css/'));
+});
+
+gulp.task('cp_assets', function () {
+    gulp.src('js/**/*.js').pipe(gulp.dest('styleguide/js/'));
+    gulp.src('css/**/*.css').pipe(gulp.dest('styleguide/css/'));
+    gulp.src('fonts/**/*.*').pipe(gulp.dest('styleguide/fonts/'));
+    gulp.src('images/**/*.*').pipe(gulp.dest('styleguide/images/'));
+});
+
+gulp.task('php2html',function(){
+    gulp.src("styleguide/index.php")
+        .pipe(php2html())
+        .pipe(gulp.dest("./"));
 });
 
 gulp.task('init', function() {
